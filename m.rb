fc1dd86ae2ad025d457a8630bdf58a0d7ccf5409
sesaba23 <sesaba23@gmail.com>
2017-06-06 16:39:49 +0200

    Added param num so we can look for one num. Added comments

--- m.rb
+++ m.rb
@@ -1,14 +1,23 @@
-def dimen(arr)
+# Returns the maximun number of times that a given number
+# appears joined, one after each other, in an array
+def dimen(arr, num)
 	dimen = 0
+	# calculate tdimen and saved it for next itreration
 	arr.inject(0) do |tdimen, x|
-		if x != 0
-			tdimen + x
+		# If num exits in the position x of the array,
+		# tdimen is incremented but only if the previous
+		# index was also num.
+		if x == num
+			tdimen + 1
 		else
+		# save tdimen only if it is bigger than the 
+		# previous saved dimen
 			dimen = tdimen unless tdimen <= dimen
 			tdimen = 0
 		end
 	end
+	# return
 	dimen
 end
 
-puts dimen([1,0,1,1,0,0,1,1,1,1,1,0,1,1,1,0])
+puts dimen([1,0,3,1,0,0,5,5,5,5,1,0,1,5,5,5,0], 5)
