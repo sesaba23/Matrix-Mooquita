6a6c8dad2f9f11d2ee1c409a3dc2acb6d6e72988
Sergio Sanchez <sesaba23@gmail.com>
2017-06-08 14:02:12 +0200

    Test file with2d arrays iteration working!

--- test.rb
+++ test.rb
@@ -1,12 +1,24 @@
-array1 = [1, 2, 3, 4, 5]
-array2 = [6, 7, 8, 9, 10]
+array1 = [1, 2]
+array2 = [6, 7]
 array3 = [11, 12, 13, 14, 15]
-array2d = [[array1],[array2],[array3]]
-array2d.each_with_index do |array, row|
-	array.each_with_index do |x, i|
-		array1[i] = x*2
-		#x * 2
+array2d = [[1,1,0],[0,1,1],[1,1,1]]
+copy = [[1,1,0],[0,1,1],[1,1,1]]
+
+time = Time.now
+for i in 0..array2d.length - 1
+	for j in 0..array2d[0].length - 1
+		if i != 0
+			if array2d[i][j] == 0
+				copy[i][j] = 0
+			else
+				copy[i][j] = copy[i][j] + copy[i-1][j]
+			end
+		end
 	end
 end
+
+time = Time.now - time
+puts "#{copy} created in #{time}ms"
+
 #puts array1
-puts array2d
+#puts array2d
